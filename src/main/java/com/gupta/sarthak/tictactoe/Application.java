97d1cb4102d0ded9079c72fb5eb11e4eac044664
DNA08 <sarthak.nitw@gmail.com>
2022-07-24 11:28:26 +0530

    Updated directory and pom file

--- src/main/java/com/gupta/sarthak/tictactoe/Application.java
+++ src/main/java/com/gupta/sarthak/tictactoe/Application.java
@@ -3,21 +3,22 @@ package com.gupta.sarthak.tictactoe;
 import com.gupta.sarthak.tictactoe.controller.GameController;
 import com.gupta.sarthak.tictactoe.exception.SymbolAlreadyInUseException;
 import com.gupta.sarthak.tictactoe.factory.player.PlayerFactory;
-import com.gupta.sarthak.tictactoe.models.*;
+import com.gupta.sarthak.tictactoe.models.Game;
+import com.gupta.sarthak.tictactoe.models.Symbol;
+import com.gupta.sarthak.tictactoe.models.User;
 import com.gupta.sarthak.tictactoe.strategies.CornerWinningStrategy;
 import com.gupta.sarthak.tictactoe.strategies.NormalWinningStrategy;
 import com.gupta.sarthak.tictactoe.strategies.RandomPlayingStrategy;
 import com.gupta.sarthak.tictactoe.strategies.WinningStrategy;
 
-import java.util.Date;
 import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
         GameController gameController = new GameController();
-        Board board = new Board(3);
-        List<Player>players = null;
+        com.gupta.sarthak.tictactoe.models.Board board = new com.gupta.sarthak.tictactoe.models.Board(3);
+        List<com.gupta.sarthak.tictactoe.models.Player>players = null;
         try {
             players = List.of(PlayerFactory.addHumanPlayer(new Symbol('X'),new User()),
                     PlayerFactory.addBot(new Symbol('O'),new RandomPlayingStrategy()));
